9d46a03f47fd1b1f9d551c5085e0f8d0a6cbf8d7
Shubham <patelsubham7777@gmail.com>
2023-03-02 12:09:36 +0530

    added method "test header" and "test_title" update.

--- test/test_login.py
+++ test/test_login.py
@@ -1,5 +1,6 @@
-import time
+from assertpy import assert_that
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class TestLoginUI:
@@ -9,6 +10,12 @@ class TestLoginUI:
         driver.implicitly_wait(20)
         driver.get("https://opensource-demo.orangehrmlive.com/")
         actual_title = driver.title
-        assert actual_title == "OrangeHRM"
-        time.sleep(5)
-        driver.quit()
+        assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(20)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        header = driver.find_element(By.XPATH, "//h5").text
+        assert_that("Login").is_equal_to(header)
