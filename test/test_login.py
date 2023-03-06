6ee38be88b0c5dba3e5d97fe7229254208aa0d4d
Shubham <patelsubham7777@gmail.com>
2023-03-06 11:52:18 +0530

    Added WebDriver Class

--- test/test_login.py
+++ test/test_login.py
@@ -1,21 +1,34 @@
+import pytest
 from assertpy import assert_that
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestLoginUI:
+class WebDriverWrapper:
+    @pytest.fixture(scope="function", autouse=True)
+    def browser_config(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+
+class TestLogin(WebDriverWrapper):
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
+
+
+class TestLoginUI(WebDriverWrapper):
     def test_title(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_title = driver.title
+        actual_title = self.driver.title
         assert_that("OrangeHRM").is_equal_to(actual_title)
 
     def test_header(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        header = driver.find_element(By.XPATH, "//h5").text
-        assert_that("Login").is_equal_to(header)
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
+        assert_that("Login").is_equal_to(actual_header)
